1b7273993b313e8bc0b8e795cab080a6d83c3c51
Tanish4181 <167396820+tanish418@users.noreply.github.com>
2025-08-07 21:24:49 +0530

    make the initial structure of the code and also add detail of extension in manifest.json file. add bootstrap files so i can use it in project.

---
+++ popup/popup.js
@@ -0,0 +1,9 @@
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stoptButton");
+
+startButton.onclick= function(){
+    console.log("TabTammer is working for you in background to minimize your distraction");
+}
+stopButton.onclick= function(){
+    console.log("TabTammer is here to help you whenever you click start");
+}
